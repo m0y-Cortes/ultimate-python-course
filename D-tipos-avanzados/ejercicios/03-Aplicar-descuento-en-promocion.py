e6e24a5ffd91436fb791a396f124962e199b8e23
m0y-Cortes <moiseskhana@gmail.com>
2026-09-21 00:47:45 -0600

    Ejercicio 03 resuelto

--- D-tipos-avanzados/ejercicios/03-Aplicar-descuento-en-promocion.py
+++ D-tipos-avanzados/ejercicios/03-Aplicar-descuento-en-promocion.py
@@ -1,5 +1,8 @@
 def aplicar_promocion(compras):
-    clientes_con_promo = [cliente for cliente, monto in compras.items() if monto > 1500]
+    clientes_con_promo = [
+        cliente for cliente, monto in compras.items() 
+        if monto > 1500
+    ]
 
 #MI SOLUCION 100% MIA
 #    cuentas_clientes_con_promo = {}
@@ -8,18 +11,19 @@ def aplicar_promocion(compras):
 #        nueva_cuenta = compras[nombre_cliente]*0.9
 #        cuentas_clientes_con_promo[nombre_cliente] = nueva_cuenta    
 #La solucion mas "elegante":
-    cuentas_clientes_con_promo = { cliente:monto for cliente, monto in compras.items() if monto > 1500}
-
-    for cliente in cuentas_clientes_con_promo:
-        cuentas_clientes_con_promo[cliente] = round(cuentas_clientes_con_promo[cliente]*0.9, 2)
+    cuentas_clientes_con_promo = {
+        cliente:round(monto*0.9, 2)
+        for cliente, monto in compras.items()
+        if monto > 1500
+    }
 
 
     return [clientes_con_promo, cuentas_clientes_con_promo]
 
-compras = {
+compras_clientes = {
     'Cliente1': 1801,
     'Cliente2': 1100,
     'Cliente3': 3000,
 }
-resultado = aplicar_promocion(compras)
+resultado = aplicar_promocion(compras_clientes)
 print(resultado)
